23bf589c07fc38a1c5d752d5825ba4698edc848d
tariqkjm7 <d.alatoum@gmail.com>
2021-10-03 18:38:39 +0300

    store page added (need module)

---
+++ src/components/Store.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import Dropdown from 'react-bootstrap/Dropdown'
+// import Form from 'react-bootstrap/Form'
+
+import Card from 'react-bootstrap/Card';
+// import axios from 'axios'
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+class Store extends React.Component {
+
+    
+
+    render() {
+
+
+
+
+        return (
+
+            <div>
+
+
+      
+
+                <Card style={{ width: '18rem' }}>
+                    <Card.Img variant="top" src={this.props.element1.thumbnail} />
+                    <Card.Body>
+                        <Card.Title>Title: {this.props.element1.title}</Card.Title>
+                        <Card.Text>
+                            <p>Rate: {this.props.element1.short_description}</p>
+
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+
+
+            </div>
+
+        )
+
+    }
+
+
+}
+
+export default Store;
