89987f853aff5e150d62ab3e1c7b3ea46983283b
Ahmad-AbuRumuh <ahmadromoh@gmail.com>
2021-10-06 22:42:26 +0300

    edit the cards

--- src/component/AboutUs.js
+++ src/component/AboutUs.js
@@ -78,6 +78,7 @@ import hesham from './images/hesham.jpg';
 import tariq from './images/tariq.jpg';
 import ismaeel from './images/ismaeel.jpg';
 import ahmad from './images/ahmad.jpg';
+import Button from 'react-bootstrap/Button'
 // import qamar from './images/qamar.jpg';
 // class teamCard extends Component {
 // static defaultProps = {
@@ -93,23 +94,23 @@ import ahmad from './images/ahmad.jpg';
 // <card name={p.name} job= {p.job} description= {p.description}
 // ))}
 
-function AboutUs({name, job, about, img}) {
+function AboutUs({ name, job, about, img }) {
   return (
     <><div id='all'>
       <div id='first'>
         <div className='card'>
-        <div className='upper-container'>
-          <div className='image-container'>
-            {/* <img src={qamar} height="100px" width="100px" alt='' /> */}
+          <div className='upper-container'>
+            <div className='image-container'>
+              {/* <img src={qamar} height="100px" width="100px" alt='' /> */}
+            </div>
           </div>
-        </div>
-        <div className="lower-container">
-          <h3>Qamar Alkhatib</h3>
-          <h4>{job}</h4>
-          <p>{about}</p>
-          <button>Visit Profile</button>
-        </div>
-      </div><div className='card'>
+          <div className="lower-container">
+            <h3>Qamar Alkhatib</h3>
+            <h4>Web Developer</h4>
+            <p>{about}</p>
+            <button>Visit Profile</button>
+          </div>
+        </div><div className='card'>
           <div className='upper-container'>
             <div className='image-container'>
               <img src={tariq} height="100px" width="100px" alt='' />
@@ -117,53 +118,60 @@ function AboutUs({name, job, about, img}) {
           </div>
           <div className="lower-container">
             <h3>Tariq Etoum</h3>
-            <h4>{job}</h4>
-            <p>{about}</p>
-            <button>Visit Profile</button>
-          </div>
-          </div>
-        </div>
-        <div className='card'>
-          <div className='upper-container'>
-            <div className='image-container'>
-              <img src={hesham} height="100px" width="100px" alt='' />
-            </div>
-          </div>
-          <div className="lower-container">
-            <h3>Hesham Hassan</h3>
-            <h4>{job}</h4>
-            <p>{about}</p>
+            <h4>Web Developer</h4>
+            <p>It's Tariq Etoum
+              I'm Software engineer and web developer
+              I've studied Java programming language
+              I graduated from ASAC
+              I'm 24 years old
+              I live in Amman Jordan
+            </p>
             <button>Visit Profile</button>
           </div>
         </div>
-        
-        </div><div className='card'>
-          <div className='upper-container'>
-            <div className='image-container'>
-              <img src={ismaeel} height="100px" width="100px" alt='' />
-            </div>
+      </div>
+      <div className='card'>
+        <div className='upper-container'>
+          <div className='image-container'>
+            <img src={hesham} height="100px" width="100px" alt='' />
           </div>
-          <div className="lower-container">
-            <h3>Ismaeel Ramadan</h3>
-            <h4>{job}</h4>
-            <p>{about}</p>
-            <button>Visit Profile</button>
+        </div>
+        <div className="lower-container">
+          <h3>Hesham Hassan</h3>
+          <h4>Web Developer</h4>
+          <p>Hello my name is Hisham Hassan I am 22 years old a student at ASAC studying software engineering A little summary about myself I like to solve many problems in general.</p>
+          <button>Visit Profile</button>
+        </div>
+
+
+      </div><div className='card'>
+        <div className='upper-container'>
+          <div className='image-container'>
+            <img src={ismaeel} height="100px" width="100px" alt='' />
           </div>
+        </div>
+        <div className="lower-container">
+          <h3>Ismaeel Ramadan</h3>
+          <h4>Web Developer</h4>
+          <p>Hi my name is Ismael Ramadan I'm 23 years old, I'm a Software Engineering student at ASAC, I studied Java programming language, and I'm a Web Developer too.</p>
+          <button href="https://github.com/IsmaelLebzo">Visit Profile</button>
+        </div>
+      </div>
+      <div className='card'>
+        <div className='upper-container'>
+          <div className='image-container'>
+            <img src={ahmad} height="100px" width="100px" alt='' />
           </div>
-        <div className='card'>
-          <div className='upper-container'>
-            <div className='image-container'>
-              <img src={ahmad} height="100px" width="100px" alt='' />
-            </div>
-          </div>
-          <div className="lower-container">
-            <h3>Ahmad AbuRumuh</h3>
-            <h4>developer</h4>
-            <p>aaaaaaaaaaaaaaaaaaaaaaaa</p>
-            <button>Visit Profile</button>
-          </div>
-        
-      </div></>
+        </div>
+        <div className="lower-container">
+          <h3>Ahmad AbuRumuh</h3>
+          <h4>Web Developer</h4>
+          <p>Hello, I'm Ahmad AbuRumuh. I have finished 2 years in software engineering at ASAC, and I took the code fellows course to be a web developer.</p>
+          <button >Visit Profile</button>
+         
+        </div>
+      </div>
+    </div></>
   )
 }
 export default AboutUs
